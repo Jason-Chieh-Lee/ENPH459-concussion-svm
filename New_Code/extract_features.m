3e4851a3fff3e3bb1552af9e43181963fae987ef
JasonChiehLee <jasonunivlee@hotmail.com>
2018-02-24 14:14:10 -0800

    Restructured code structure to seperate new and old code

---
+++ New_Code/extract_features.m
@@ -0,0 +1,12 @@
+function [feature_matrix] = extract_features(eeg_mat)
+%EXTRACT_FEATURES Summary of this function goes here
+%   Detailed explanation goes here
+
+eeg_struct = load(eeg_mat);
+field = fieldnames(eeg_struct);
+field_name = field{1};
+eeg_matrix = eeg_struct.(field_name);
+
+feature_matrix = eeg_matrix;
+end
+
--- Test Scripts/feature_extraction.m
+++
@@ -1,4 +0,0 @@
-eeg_struct = load('BN.mat');
-field = fieldnames(eeg_struct);
-field_name = field{1};
-eeg_matrix = eeg_struct.(field_name);
