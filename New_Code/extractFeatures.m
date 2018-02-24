6000432c42f8ef8186d31daefea5f9b7b3e2b2cc
Jason Chieh Lee <jasonunivlee@hotmail.com>
2018-02-24 15:56:24 -0800

    Added test script for testing functions and Shannon Entropy Framework

--- New_Code/extractFeatures.m
+++ New_Code/extractFeatures.m
@@ -1,14 +1,15 @@
-function [feature_matrix] = extract_features(eeg_mat)
+function [featureMatrix] = extractFeatures(eegMat)
 %EXTRACT_FEATURES Summary of this function goes here
 %   Detailed explanation goes here
 
-eeg_struct = load(eeg_mat);
-field = fieldnames(eeg_struct);
-field_name = field{1};
-eeg_matrix = eeg_struct.(field_name);
+eegStruct = load(eegMat);
+field = fieldnames(eegStruct);
+fieldName = field{1};
+eegMatrix = eegStruct.(fieldName);
 % Power Spectral Analysis
 % Wavelet Decomposition Analysis
 % Shannon Entropy Analysis
-feature_matrix = eeg_matrix;
+shannonEntropyMatrix = getShannonEntropy(eegMat);
+featureMatrix = eegMatrix;
 end
 
---
+++ New_Code/test.m
@@ -0,0 +1,1 @@
+extractFeatures('BN.mat');
