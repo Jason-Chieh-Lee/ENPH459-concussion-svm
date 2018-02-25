7d31b4b968536f33801b5bffdb4985e6735216db
Jason Chieh Lee <jasonunivlee@hotmail.com>
2018-02-24 16:33:25 -0800

    Added documentation for extractFeatures and getShannonEntropy

--- New_Code/extractFeatures.m
+++ New_Code/extractFeatures.m
@@ -1,6 +1,15 @@
 function [featureMatrix] = extractFeatures(eegMat)
-%EXTRACT_FEATURES Summary of this function goes here
-%   Detailed explanation goes here
+%EXTRACT_FEATURES Takes in the .mat file of EEG and returns a matrix
+%containing 30 features (row) per channel (column)
+
+%   This function takes in the EEG data as a .mat file, converts it to a
+%   matlab matrix object then calls on power spectral analysis, Wavelet
+%   decomposition, and shannon entropy functions to get a matrix back from
+%   each function. Then the matrices are combined into a feature matrix and
+%   returned.
+%   Input: eegMat - .mat file containing the EEG data
+%   Output: featureMatrix - 30x27 matrix containing features (row) of each
+%   channel (column)
 
 eegStruct = load(eegMat);
 field = fieldnames(eegStruct);
--- New_Code/getShannonEntropy.m
+++ New_Code/getShannonEntropy.m
@@ -1,9 +1,14 @@
 function shannonEntropyMatrix = getShannonEntropy(eegMatrix)
-%GETSHANNONENTROPY Summary of this function goes here
-%   Detailed explanation goes here
+%GETSHANNONENTROPY Takes in matrix of EEG data and returns a 1x27 matrix
+% with Shannon Entropy of each channel.
+%   Input: eegMatrix - a nx27 matrix of the eeg data for each channel
+%   Output: shannonEntropyMatrix - a 1x27 matrix of the shannon entropy of
+%   each channel. This can then be added to the feature matrix.
+
 [rowSize, colSize] = size(eegMatrix);
 shannonEntropyMatrix = zeros(1,colSize);
 
+% Get Shannon entropy of each channel (column)
 for col = 1:colSize
     shannonEntropyMatrix(1,col) = wentropy(eegMatrix(1,col),'shannon');
 end
