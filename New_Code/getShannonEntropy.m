cd3ab901f96b95d51f775ee4ce146051025f8620
Jason Chieh Lee <jasonunivlee@hotmail.com>
2018-02-24 16:20:10 -0800

    Finished Shannon Entropy Script

--- New_Code/getShannonEntropy.m
+++ New_Code/getShannonEntropy.m
@@ -2,6 +2,11 @@ function shannonEntropyMatrix = getShannonEntropy(eegMatrix)
 %GETSHANNONENTROPY Summary of this function goes here
 %   Detailed explanation goes here
 [rowSize, colSize] = size(eegMatrix);
-shannonEntropyMatrix = eegMatrix;
+shannonEntropyMatrix = zeros(1,colSize);
+
+for col = 1:colSize
+    shannonEntropyMatrix(1,col) = wentropy(eegMatrix(1,col),'shannon');
+end
+
 end
 
