9dcd5b0d0e5c5a01fe5a5decc500041c33325464
Jason Chieh Lee <jasonunivlee@hotmail.com>
2018-03-18 17:20:57 -0700

    Finished Shannon Entropy code

--- New_Code/test.m
+++ New_Code/test.m
@@ -1,1 +1,7 @@
-extractFeatures('BN.mat');
+eegStruct = load('BN.mat');
+field = fieldnames(eegStruct);
+fieldName = field{1};
+eegMatrix = eegStruct.(fieldName);
+matrix = [1 1;1 1;2 1;2 1;3 2;3 3];
+[shannonmat, uniquesymbols, probmatrix] = getShannonEntropy(eegMatrix);
+%[shannonmat, uniquesymbols, probmatrix] = getShannonEntropy(matrix)
