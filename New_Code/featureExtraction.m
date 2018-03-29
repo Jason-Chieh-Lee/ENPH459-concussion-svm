67b946752578166259b01a2aa938709741da8eeb
jamiec-ye <jamiec.ye@gmail.com>
2018-03-29 00:46:52 -0700

    Minor change to elimiate file path hardcode.

--- New_Code/featureExtraction.m
+++ New_Code/featureExtraction.m
@@ -4,8 +4,6 @@
 
 function [featureMatrix,labels] = featureExtraction(directory)
 
-%directory = 'C:\Users\Dylan\Desktop\New folder\Capstone Training Data';
-
 featureMatrix = zeros(1, 891); %33x27 features extracted for each patient
 
 %Extract .mat files corresponding to concussed patients 
